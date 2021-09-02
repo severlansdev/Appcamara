1d3ac2d5d44ba8faf0eee8556c47a14c312c7603
ENRIQUE PARRA <enrique.parra@ingenian.com>
2021-09-01 20:21:19 -0500

    primera fase aplicacion camara

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Camera} from 'expo-camera';
 
 export default function App() {
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHaspermission] = useState(null);
+
+  useEffect(() => {
+    (async() =>{
+      const { status } = await Camera.requestPermissionsAsync();
+      setHaspermission(status === 'granted');
+    })();
+  }, []);
+
+  if(hasPermission === null){
+    return <View/>;
+  }
+
+  if(hasPermission === false){
+    return <Text> Acceso denegado </Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+      >
+      <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row'}}>
+        <TouchableOpacity 
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          left: 20,
+        }}
+        onpress={ () => {
+          setType(
+            type === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front 
+            : Camera.Constants.Type.back
+            );
+        }}>
+          <Text style={{ fontSize: 20, marginBottom: 13, color: '#fff'}}>cambiar</Text>
+        </TouchableOpacity>
+      </View>
+      </Camera>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
